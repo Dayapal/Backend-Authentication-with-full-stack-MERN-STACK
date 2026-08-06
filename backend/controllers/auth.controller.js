f1e3f6344dea4b0f394ac586c58d0eefc5c64df2
Daya Pal <dayapal12pc@gmail.com>
2026-08-06 17:49:10 +0530

    added user register

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,56 +1,57 @@
 import bcrypt from "bcryptjs";
 import User from "../model/user.model.js";
 import generateToken from "../utils/generateToken.js";
+import { Suspense } from "react";
 
 
-export const register = async (req, res) => {
-  try {
+// export const register = async (req, res) => {
+//   try {
    
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Please fill all the fields",
-      });
-    }
-    const existingUser = await User.findOne({ email });
+//     const { name, email, password } = req.body;
+//     if (!name || !email || !password) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Please fill all the fields",
+//       });
+//     }
+//     const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-      return res.status(409).json({
-        success: false,
-        message: "User already exists",
-      });
-    }
-
-   
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-
-    const user = new User({
-      name,
-      email,
-      password: hashedPassword,
-    });
+//     if (existingUser) {
+//       return res.status(409).json({
+//         success: false,
+//         message: "User already exists",
+//       });
+//     }
 
    
-    await user.save();
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+
+//     const user = new User({
+//       name,
+//       email,
+//       password: hashedPassword,
+//     });
+
+   
+//     await user.save();
+//     res.status(201).json({
+//       success: true,
+//       message: "User registered successfully",
+//       data: {
+//         id: user._id,
+//         name: user.name,
+//         email: user.email,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 
 
 
@@ -102,6 +103,56 @@ export const login = async (req, res) => {
 };
 
 
+
+
+export const register = async(req,res) =>{
+  try {
+    const {name,email,password} = req.body;
+    if(!name || !email || !password){
+      return res.status(404).json({
+        success: false,
+        message : "All fields are required"
+      })
+    }
+    const existingUser = await User.findOne({email});
+
+    if(existingUser){
+       return res.status(400).json({
+        success: false,
+        message : "This mail is already exists",
+       })
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const hashPassword = await bcrypt.hash(password,salt);
+
+    const user = new User({
+      name,
+      email,
+      password: hashPassword
+    })
+
+     await user.save();
+     res.status(201).json({
+      success: true,
+      message: "User register successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      }
+     })
+    
+    
+  } catch (error) {
+    console.log("Failed to register User", error.message);
+    res.status(400).json({
+      success: false,
+      message: "Failed to register User",
+      error: error.message
+    })
+  }
+}
 
 
 
