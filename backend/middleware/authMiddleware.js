7bcc6178b4b3e54b9a35885ffd3105ae9204cb5b
Daya Pal <dayapal12pc@gmail.com>
2026-08-08 21:37:09 +0530

    feat: add second authentication middleware with improved error handling

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -32,3 +32,44 @@ const authMiddleware = async (req, res, next) => {
 };
 
 export default authMiddleware;
+
+
+
+
+
+
+
+
+export const authMiddleware2 = async(req,res,next) =>{
+  try {
+    const authHeader = req.headers.authorization;
+    
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+      return res.status(404).json({
+        success: false,
+        message: "Token not Provided"
+      })
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+    if(!user){
+      return res.status(400).json({
+        success: false,
+        message : "User not Found"
+      })
+    };
+
+    req.user = user;
+    next();
+    
+  } catch (error) {
+    console.log("Invalid Token and Authorzation");
+    res.status(400).json({
+      success: false,
+      message : "Invalied and Token Expired"
+    })
+    
+  }
+}
