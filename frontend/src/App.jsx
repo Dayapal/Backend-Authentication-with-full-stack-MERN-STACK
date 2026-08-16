7ac801412f0fa478d1d9c2e5ab772185d34ff253
Daya Pal <dayapal12pc@gmail.com>
2026-08-16 10:42:25 +0530

    fix: remove unnecessary line breaks in routing for cleaner code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,12 +12,11 @@ const App = () => {
 
     <BrowserRouter>
       <Routes>
-        <Route path='/'
-          element={<Navigate to="/login" />}
-        />
+        <Route path='/'element={<Navigate to="/login" />}/>
         <Route path='/register'element={<Register />}/>
 
         <Route path="/login" element={<Login />} />
+        
         <Route
           path='/dashboard'
           element={
