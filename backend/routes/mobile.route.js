a607ae6c5e4ee8a1d38c9067ec5c89cbb74b8f32
Daya Pal <dayapal12pc@gmail.com>
2026-08-19 18:35:59 +0530

    feat: add auth middleware to mobile creation route

--- backend/routes/mobile.route.js
+++ backend/routes/mobile.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { createMobile, getMobiles } from '../controllers/mobile.controller.js';
+import authMiddleware from '../middleware/authMiddleware.js'
 
 
 export const mobileRoute = express.Router();
-mobileRoute.post("/mobile", createMobile)
+mobileRoute.post("/mobile",  authMiddleware,  createMobile)
 mobileRoute.get("/mobiles", getMobiles)
