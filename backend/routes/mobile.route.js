177e46e224661fa4486c41496c99633488072ec1
Daya Pal <dayapal12pc@gmail.com>
2026-09-01 10:38:41 +0530

    refactor: remove commented authMiddleware import for cleaner code

--- backend/routes/mobile.route.js
+++ backend/routes/mobile.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import multer from "multer";
 import {createMobile,getMobiles,} from "../controllers/mobile.controller.js";
-// import authMiddleware from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 export const mobileRoute = express.Router();
 mobileRoute.post("/mobile",  upload.single("image"),createMobile);
